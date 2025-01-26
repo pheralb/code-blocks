28159681be62c54aabc627713e21c9e89f96a254
pheralb <pablohdez4340@gmail.com>
2025-01-26 00:34:00 +0000

    docs: initial page (prepare)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
-export default function Home() {
-  return (
-    <div>
-      <h2 className="font-medium text-2xl">hello</h2>
-    </div>
-  );
+import type { Metadata } from "next";
+
+import { allDocs } from "content-collections";
+import { notFound } from "next/navigation";
+import { MDX } from "@/mdx/mdx";
+
+const indexPage = "index";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const document = allDocs.find((post) => post.slug === indexPage);
+
+  if (!document) {
+    return notFound();
+  }
+
+  return {
+    title: `${document.title} - @pheralb/codeblocks`,
+    description: document.description,
+  };
 }
+
+const Page = () => {
+  const document = allDocs.find((post) => post.slug === indexPage);
+
+  if (!document) {
+    return notFound();
+  }
+
+  return (
+    <>
+      <MDX code={document.mdx} />
+    </>
+  );
+};
+
+export default Page;
