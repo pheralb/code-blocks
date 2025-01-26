c5062c3c19ad8016f58328dec911fc9a579e1021
pheralb <pablohdez4340@gmail.com>
2025-01-26 01:50:07 +0000

    feat: add ``ComponentPreview`` and ``ComponentSource`` to custom MDX components

--- src/mdx/components.tsx
+++ src/mdx/components.tsx
@@ -1,6 +1,10 @@
 import type { MDXComponents } from "mdx/types";
+
 import CodeblockMDX from "@/components/codeblock/mdx";
+import { ComponentPreview, ComponentSource } from "@/components/sourcePreview";
 
 export const MDXCustomComponents: MDXComponents = {
   pre: (props) => <CodeblockMDX {...props} />,
+  ComponentPreview,
+  ComponentSource,
 };
