809252bd324b08b10a0c5634f9aefee7540ebeec
Pablo Hdez <62877300+pheralb@users.noreply.github.com>
2025-03-21 14:13:51 +0000

    feat: create mdx components + rehype/shiki options

---
+++ src/mdx/rehypeShiki.ts
@@ -0,0 +1,8 @@
+import type { RehypeShikiOptions } from "@shikijs/rehype";
+
+export const rehypeShikiOptions: RehypeShikiOptions = {
+  themes: {
+    light: "catppuccin-latte",
+    dark: "vesper",
+  },
+};
