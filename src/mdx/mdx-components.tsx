180fa1a931443e157c1142751103d4802426c65d
pheralb <pablohdez4340@gmail.com>
2025-03-23 13:15:17 +0000

    feat: add separator mdx component

--- src/mdx/mdx-components.tsx
+++ src/mdx/mdx-components.tsx
@@ -1,7 +1,12 @@
 import type { MDXComponents } from "mdx/types";
+
 import { Code, Codeblock } from "@/codeblock/mdx";
+import { MDXTypography } from "@/ui/typography";
+import Separator from "@/ui/separator";
 
 export const MDXCustomComponents: MDXComponents = {
   pre: (props) => <Codeblock showlang="bottom" {...props} />,
   code: (props) => <Code {...props} />,
+  Separator,
+  ...MDXTypography,
 } satisfies MDXComponents;
