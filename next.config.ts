2b3e4d8154006b0c178b98b5758ecd8a74787538
pheralb <pablohdez4340@gmail.com>
2025-03-23 13:09:52 +0000

    feat: use experimental ``viewTransition`` API

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import { withContentCollections } from "@content-collections/next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    viewTransition: true,
+  },
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
