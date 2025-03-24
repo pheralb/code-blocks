b2642e7064a084e21b62488d03cc4f4d6069e408
pheralb <pablohdez4340@gmail.com>
2025-03-24 14:12:03 +0000

    fix: improve mdx typography

--- src/ui/typography.tsx
+++ src/ui/typography.tsx
@@ -2,13 +2,16 @@ import type { MDXComponents } from "mdx/types";
 
 import Link from "next/link";
 import ExternalLink from "./externalLink";
+import { cn } from "@/utils/cn";
 
 export const MDXTypography: MDXComponents = {
   a: ({ href, ...props }) => {
     if (href.startsWith("https://") || href.startsWith("http://")) {
-      return <ExternalLink href={href} {...props} />;
+      return (
+        <ExternalLink className={cn("underline")} href={href} {...props} />
+      );
     }
-    return <Link href={href} {...props} />;
+    return <Link className={cn("underline")} href={href} {...props} />;
   },
   h1: (props) => (
     <h1
@@ -18,13 +21,13 @@ export const MDXTypography: MDXComponents = {
   ),
   h2: (props) => (
     <h2
-      className="font-headings scroll-m-20 pb-3 text-3xl font-medium tracking-tight first:mt-0"
+      className="font-headings mt-10 scroll-m-20 border-b border-neutral-200 pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0 dark:border-neutral-800"
       {...props}
     />
   ),
   h3: (props) => (
     <h3
-      className="font-headings scroll-m-20 text-2xl font-semibold tracking-tight"
+      className="font-headings mt-8 scroll-m-20 text-2xl font-semibold tracking-tight"
       {...props}
     />
   ),
@@ -36,13 +39,13 @@ export const MDXTypography: MDXComponents = {
   ),
   p: (props) => (
     <p
-      className="leading-7 text-pretty [&:not(:first-child)]:mt-6"
+      className="leading-7 text-balance text-neutral-900 dark:text-neutral-100 [&:not(:first-child)]:mt-6"
       {...props}
     />
   ),
   strong: (props) => <strong className="font-bold" {...props} />,
   blockquote: (props) => (
-    <blockquote className="mt-6 border-l-2 pl-6 italic" {...props} />
+    <blockquote className="border-l-2 pl-6 italic" {...props} />
   ),
   ul: (props) => <ul className="my-6 ml-6 list-disc [&>li]:mt-2" {...props} />,
 };
