9f1264d09bcb23bd24002317372cb6b7b5be9f66
pheralb <pablohdez4340@gmail.com>
2025-03-26 10:17:59 +0000

    feat: improve icons data structure; add new icons for Document and CSS, and adjust icon size

--- src/codeblock/icons.tsx
+++ src/codeblock/icons.tsx
@@ -1,22 +1,50 @@
-import { BracketsGreen, Js, Reactts, TypeScript } from "@react-symbols/icons";
+import type { JSX } from "react";
 
-const iconSize = 20;
+import {
+  Js,
+  BracketsGreen,
+  Document,
+  Reactts,
+  TypeScript,
+  BracketsBlue,
+} from "@react-symbols/icons";
 
-export const icons = [
+const iconSize = 16;
+
+export interface IconsData {
+  lang?: string;
+  name: string;
+  icon: JSX.Element;
+}
+
+export const icons: IconsData[] = [
+  {
+    name: "Document",
+    icon: <Document width={iconSize} height={iconSize} />,
+  },
   {
     lang: "tsx",
+    name: "React",
     icon: <Reactts width={iconSize} height={iconSize} />,
   },
   {
     lang: "ts",
+    name: "TypeScript",
     icon: <TypeScript width={iconSize} height={iconSize} />,
   },
   {
     lang: "js",
+    name: "JavaScript",
     icon: <Js width={iconSize} height={iconSize} />,
   },
   {
     lang: "bash",
+    name: "Terminal",
     icon: <BracketsGreen width={iconSize} height={iconSize} />,
-  }
+  },
+  {
+    lang: "css",
+    name: "CSS",
+    icon: <BracketsBlue width={iconSize} height={iconSize} />,
+  },
 ];
