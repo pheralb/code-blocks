58166dac0e3b5756876dcf24536010f0b74eded2
pheralb <pablohdez4340@gmail.com>
2025-11-12 19:59:12 +0000

    feat: initial global sidebar content

---
+++ src/components/layout/sidebarAppContent.tsx
@@ -0,0 +1,13 @@
+import { Sidebar, SidebarContent } from "../ui/sidebar";
+
+const SidebarAppContent = () => {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <h2>Content</h2>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
+
+export default SidebarAppContent;
