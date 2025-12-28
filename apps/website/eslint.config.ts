6604f26d34e372bfe194a36607afea9e8e1d75ef
pheralb <pablohdez4340@gmail.com>
2025-12-28 19:25:27 +0000

    feat: add custom eslint package

--- apps/website/eslint.config.ts
+++ apps/website/eslint.config.ts
@@ -1,35 +1,9 @@
-import { fileURLToPath } from "node:url";
-import { includeIgnoreFile } from "@eslint/compat";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 
-// Plugins:
-import nextTs from "eslint-config-next/typescript";
-import prettier from "eslint-config-prettier/flat";
-import nextVitals from "eslint-config-next/core-web-vitals";
+// Package Plugins:
+import { baseConfig } from "@pheralb/cb-eslint/base";
+import { nextConfig } from "@pheralb/cb-eslint/nextjs";
 
-// Gitignore:
-const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
-
-const eslintConfig = defineConfig([
-  includeIgnoreFile(gitignorePath),
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-  ...nextVitals,
-  ...nextTs,
-  prettier,
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-        },
-      ],
-    },
-    linterOptions: {
-      reportUnusedDisableDirectives: "warn",
-    },
-  },
-]);
+const eslintConfig = defineConfig(baseConfig, nextConfig);
 
 export default eslintConfig;
