3b73a68af80662e6875d14d883510563caca2aef
pheralb <pablohdez4340@gmail.com>
2026-01-04 14:47:44 +0000

    feat: fixed truncate item

--- apps/website/src/components/docs/toc-menu.tsx
+++ apps/website/src/components/docs/toc-menu.tsx
@@ -5,7 +5,7 @@ import type { ToCItem } from "@/mdx/plugins/generateToC";
 import Link from "next/link";
 import { cn } from "@/utils/cn";
 import { useEffect, useState } from "react";
-import { TextAlignStartIcon } from "lucide-react";
+import { CornerDownRightIcon, TextAlignStartIcon } from "lucide-react";
 
 interface TableOfContentsProps {
   data: ToCItem[];
@@ -47,7 +47,7 @@ const TableOfContents = ({ data }: TableOfContentsProps) => {
 
   return (
     <div className="px-7 py-6">
-      <div className="mb-2 flex items-center space-x-2.5 text-neutral-600 dark:text-neutral-400">
+      <div className="mb-3 flex items-center space-x-2.5 text-neutral-600 dark:text-neutral-400">
         <TextAlignStartIcon size={16} />
         <h2 className="text-sm font-semibold">On this page</h2>
       </div>
@@ -59,15 +59,17 @@ const TableOfContents = ({ data }: TableOfContentsProps) => {
               title={item.text}
               href={`#${item.slug}`}
               className={cn(
-                "max-w-48 truncate transition-colors",
-                "border-l",
-                item.level === 2 && "py-1",
+                "max-w-48 py-1 transition-colors",
+                "flex items-center space-x-2 border-l",
+                item.level === 2 && "pl-3.5",
+                item.level === 3 && "pl-4",
                 activeId === item.slug
                   ? "border-neutral-200 font-medium text-neutral-900 dark:border-neutral-400 dark:text-neutral-100"
                   : "border-neutral-300 text-neutral-600 hover:text-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-neutral-100",
               )}
             >
-              <span className="pl-3">{item.text}</span>
+              {item.level >= 3 && <CornerDownRightIcon size={12} />}
+              <span className="truncate">{item.text}</span>
             </Link>
           ))}
       </nav>
