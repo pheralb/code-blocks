3638654f8ba4ed09a0367e109e3493d13f1caa0b
pheralb <pablohdez4340@gmail.com>
2026-02-12 23:50:57 +0000

    fix: update border color for active table of contents item with light mode

--- apps/website/src/components/docs/toc-menu.tsx
+++ apps/website/src/components/docs/toc-menu.tsx
@@ -64,7 +64,7 @@ const TableOfContents = ({ data }: TableOfContentsProps) => {
                 item.level === 2 && "pl-3.5",
                 item.level === 3 && "pl-4",
                 activeId === item.slug
-                  ? "border-neutral-200 font-medium text-neutral-900 dark:border-neutral-400 dark:text-neutral-100"
+                  ? "border-neutral-600 font-medium text-neutral-900 dark:border-neutral-400 dark:text-neutral-100"
                   : "border-neutral-300 text-neutral-600 hover:text-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-neutral-100",
               )}
             >
