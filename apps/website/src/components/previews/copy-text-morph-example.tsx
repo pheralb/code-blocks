e604b90e2e168dcb9a72d2685a2502d91d8fe1bf
pheralb <pablohdez4340@gmail.com>
2026-02-17 15:21:14 +0000

    docs: update copy-text-morph block + doc example

--- apps/website/src/components/previews/copy-text-morph-example.tsx
+++ apps/website/src/components/previews/copy-text-morph-example.tsx
@@ -8,27 +8,22 @@ import {
 import { CodeblockShiki } from "@/components/code-block/client/shiki";
 import { CopyTextMorph } from "@/components/code-block/blocks/copy-text-morph";
 
-const code = `const copyToClipboard = async (text: string) => {
-  try {
-    await navigator.clipboard.writeText(text);
-    console.log("Text copied to clipboard");
-  } catch (err) {
-    console.error("Failed to copy text: ", err);
-  }
-};`;
+const code = `export default function Page() {
+  return <h1>Hello</h1>
+}`;
 
 const CopyMorphExample = () => {
   return (
     <CodeBlock>
       <CodeBlockHeader>
         <CodeBlockGroup>
-          <CodeBlockIcon language="js" />
+          <CodeBlockIcon language="tsx" />
           <span>Copy with Text Morph</span>
         </CodeBlockGroup>
         <CopyTextMorph content={code} />
       </CodeBlockHeader>
       <CodeBlockContent>
-        <CodeblockShiki language="ts" code={code} />
+        <CodeblockShiki language="tsx" code={code} />
       </CodeBlockContent>
     </CodeBlock>
   );
